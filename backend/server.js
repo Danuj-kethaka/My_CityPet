92d4ae40d9c203c2e8c58c56db320e1860864dca
Danuj-kethaka <senanayakakethaka@gmail.com>
2026-04-21 09:59:40 +0530

    vercel deleted

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-//app.listen(PORT, () => {
-//  connectDB();
-//  console.log("Server started at http://localhost:" + PORT);
-//});
+app.listen(PORT, () => {
+connectDB();
+console.log("Server started at http://localhost:" + PORT);
+});
